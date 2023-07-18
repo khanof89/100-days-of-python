3cde8fead733aaf796b28c11852c0a03592d8429
Shahrukh Khan <khan_shahrukh89@yahoo.com>
2023-07-18 22:17:40 +0200

    day 2 exercise

--- day-2/tip-calculator.py
+++ day-2/tip-calculator.py
@@ -1,6 +1,7 @@
+import math
 print('Welcome to tip calculator.')
-bill = int(input('What was the total bill?'))
-people = int(input('How many people to split the bill'))
-tip = int(input('What percentage tip would you like to give?'))
+bill = float(input('What was the total bill?'))
+people = float(input('How many people to split the bill'))
+tip = float(input('What percentage tip would you like to give?'))
 amount_to_pay = (bill+(bill * tip /100)) / people
-print('Each person should pay: $', amount_to_pay)
+print('Each person should pay: $', math.ceil(amount_to_pay))
